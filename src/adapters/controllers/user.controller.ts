dba262ae35233a1c7630f4f8cf0948cbe43f1883
KUNAL YEOLA <KunYeo@RC.local>
2026-05-25 15:59:08 +0530

    changes

--- src/adapters/controllers/user.controller.ts
+++ src/adapters/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import { Request , Response } from "express";
 
 export const gerUser = async (req: Request, resp: Response) =>{
     try {
-        return resp.status(200).send("Success to deploy CICD")
+        return resp.status(200).send("Success to deploy CICDT")
     } catch (error) {
         return resp.status(400).
         send("sorry")
