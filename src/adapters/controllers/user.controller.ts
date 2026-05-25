4aa5ef53c6288230b7a62d26dd45c0cf4348f29d
KUNAL YEOLA <KunYeo@RC.local>
2026-05-25 12:52:03 +0530

    Deploying the code using CICD

--- src/adapters/controllers/user.controller.ts
+++ src/adapters/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import { Request , Response } from "express";
 
 export const gerUser = async (req: Request, resp: Response) =>{
     try {
-        return resp.status(200).send("Success")
+        return resp.status(200).send("Success to deploy CICD")
     } catch (error) {
         return resp.status(400).
         send("sorry")
